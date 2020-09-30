ed53603a3e6fa2b15a468388607bc2e615e5c70d
kumarSnake <hi.p@outlook.com>
2020-09-30 08:20:33 +0530

    Simple explanation with java regex link

--- java/easy/JavaStringTokens.java
+++ java/easy/JavaStringTokens.java
@@ -5,7 +5,34 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/java-string-tokens/problem
  * 
  * This code is based upon user pmtatar https://www.hackerrank.com/pmtatar
+ * Regex expression constructs https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html#sum
  * 
+ * method signature
+ * String[] string.split(regex);
+ * 
+ * you write the regex pattern for split 
+ * In this case !,?._'@\\s
+ * 
+ * here \\s denotes A whitespace character: [ \t\n\x0B\f\r]
+ * the \ is for escape sequence, \s for Whitespace
+ * 
+ * [] represent a character class ie group of character to match
+ * if any you can write range in this class eg [a-z0-9]
+ * a to z 0 to 9
+ * 
+ * () explicit capture, capture entire, capture group
+ * eg (a-z) it will be no range but only capture
+ * "a-z" string.
+ * 
+ * \R Linebreak matcher 
+ * 
+ * greedy Quantifiers
+ * x? x once or not at all
+ * x* x zero or more times
+ * x+ x one or more times
+ * x{n} x exactly n times
+ * x(n,) x at least n times
+ * x(n,m) x at least n but not more than m times
  * 
  */
 public class JavaStringTokens {
